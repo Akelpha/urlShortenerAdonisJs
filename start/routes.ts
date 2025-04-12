04e3c751d8730986a412e0114b908a909f2c8f66
Akelpha <kerennzira@gmail.com>
2025-04-12 21:37:57 +0100

    feat: update dependencies and enhance return page layout
    - Added "edge" dependency to package.json for improved template rendering.
    - Updated favicon link in app.edge to point to the correct public path.
    - Refactored return.edge to improve layout and structure, including:
      - Wrapped navigation links in a div for better styling.
      - Uncommented and restructured main content to display short URL and QR code.
      - Added conditional rendering for flash messages to handle errors and success states.
    - Updated route definition for the about page to include a named route.

--- start/routes.ts
+++ start/routes.ts
@@ -3,6 +3,5 @@ import router from '@adonisjs/core/services/router'
 const UrlController = () => import('#controllers/url_controller')
 
 router.on('/').render('home')
-
 router.post('/return', [UrlController, 'index'])
-router.get('/about', [UrlController, 'about'])
+router.get('/about', [UrlController, 'about']).as('about')
